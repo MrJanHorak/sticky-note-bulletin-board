3c36b8f7ea4fa61490634587763b4829538cb668
MrJanHorak <janny_b_goode@hotmail.com>
2021-11-29 13:34:26 -0500

    fix containment issue with draggable elements

--- main_app/static/js/draggability.js
+++ main_app/static/js/draggability.js
@@ -92,13 +92,18 @@ $(function () {
   $.each(positions, function (id, pos) {
     $("#" + id).css(pos);
   });
-
+  
   d.draggable({
     containment: "#containment-wrapper",
     scroll: false,
+    opacity: 0.4,
+    stack: ".products",
+    zIndex: 100,
+    containment: "window",
     stop: function (event, ui) {
       positions[this.id] = ui.position;
       localStorage.positions = JSON.stringify(positions);
     },
   });
 });
+
